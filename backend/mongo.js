4b71fbc0c4da4f9955c5b123fac1bb3f425cc402
01-SumitKumar <sumitkumar28779@gmail.com>
2024-01-03 18:37:39 +0530

    first commit

---
+++ backend/mongo.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://sumit:19103521%40Amit@cluster0.ovtdlua.mongodb.net/ecommerce")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
